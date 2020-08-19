ac0d2c3f850c1bdc1bca19c53d445567e8a5d4dc
Karthik <karthik@testpress.in>
2020-08-19 16:48:52 +0530

    Remove jobs prefix

--- apps/jobs/models.py
+++ apps/jobs/models.py
@@ -12,15 +12,22 @@ from model_utils import Choices
 class Job(TimeStampedModel, TimeFramedModel):
     STATUS = Choices('not_started', 'queued', 'started', 'processing', 'completed', 'cancelled', 'error')
 
-    job_template = models.ForeignKey("presets.JobTemplate", null=True, on_delete=models.SET_NULL)
-    job_settings = models.JSONField("Job Settings", null=True)
-    job_id = models.UUIDField("Job Id", primary_key=True, default=uuid.uuid4, editable=False, db_index=True)
+    id = models.UUIDField("Job Id", primary_key=True, default=uuid.uuid4, editable=False, db_index=True)
+    template = models.ForeignKey("presets.JobTemplate", null=True, on_delete=models.SET_NULL)
+    settings = models.JSONField("Job Settings", null=True)
     background_task_id = models.UUIDField(
         "Background Task Id", default=uuid.uuid4, db_index=True, null=True, max_length=255
     )
     progress = models.PositiveSmallIntegerField("Progress", default=0)
     status = StatusField()
     input_url = models.CharField("Input URL", max_length=1024)
+    output_url = models.CharField("Output URL", max_length=1024)
+
+    class Meta:
+        ordering = ("-created",)
+
+    def __str__(self):
+        return self.job_id
 
 
 class Output(AbstractOutputPreset):
