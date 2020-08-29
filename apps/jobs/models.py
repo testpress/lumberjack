95b7720f36a41bb2144560ace2ef095c9e7b396e
Karthik <karthik@testpress.in>
2020-08-29 11:44:41 +0530

    Add models

---
+++ apps/jobs/models.py
@@ -0,0 +1,60 @@
+import uuid
+
+from django.db import models
+
+from apps.presets.models import AbstractOutputPreset
+
+from model_utils.models import TimeStampedModel, TimeFramedModel
+from model_utils.fields import StatusField
+from model_utils import Choices
+
+
+class Job(TimeStampedModel, TimeFramedModel):
+    STATUS = Choices(
+        ("not_started", "Not Started"),
+        ("queued", "Queued"),
+        ("started", "Started"),
+        ("processing", "Processing"),
+        ("completed", "Completed"),
+        ("cancelled", "Cancelled"),
+        ("error", "Error"),
+    )
+
+    id = models.UUIDField("Job Id", primary_key=True, default=uuid.uuid4, editable=False, db_index=True)
+    template = models.ForeignKey("presets.JobTemplate", null=True, on_delete=models.SET_NULL)
+    settings = models.JSONField("Job Settings", null=True)
+    background_task_id = models.UUIDField(
+        "Background Task Id", default=uuid.uuid4, db_index=True, null=True, max_length=255
+    )
+    progress = models.PositiveSmallIntegerField("Progress", default=0)
+    status = StatusField()
+    input_url = models.CharField("Input URL", max_length=1024)
+    output_url = models.CharField("Output URL", max_length=1024)
+    webhook_url = models.URLField("Webhook URL")
+
+    class Meta:
+        ordering = ("-created",)
+
+    def __str__(self):
+        return f"JOB {self.id} - {self.get_status_display()}"
+
+
+class Output(AbstractOutputPreset):
+    STATUS = Choices(
+        ("not_started", "Not Started"),
+        ("queued", "Queued"),
+        ("started", "Started"),
+        ("processing", "Processing"),
+        ("completed", "Completed"),
+        ("cancelled", "Cancelled"),
+        ("error", "Error"),
+    )
+
+    job = models.ForeignKey(Job, null=True, on_delete=models.SET_NULL)
+    status = StatusField()
+    progress = models.PositiveSmallIntegerField("Progress", default=0)
+    background_task_id = models.UUIDField(
+        "Background Task Id", default=uuid.uuid4, db_index=True, null=True, max_length=255
+    )
+    settings = models.JSONField("Settings", null=True)
+    error_message = models.TextField("Error Message", null=True, blank=True)
---
+++ apps/presets/models.py
@@ -0,0 +1,54 @@
+import uuid
+
+from django.db import models
+from django.utils.text import slugify
+
+from model_utils.models import TimeStampedModel
+
+
+class JobTemplate(TimeStampedModel):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField("Name", max_length=255, unique=True)
+    slug = models.SlugField("Slug", max_length=255, unique=True, blank=True, db_index=True)
+    settings = models.JSONField("Settings", null=True)
+    destination = models.CharField("Destination", max_length=1024)
+    segment_length = models.PositiveSmallIntegerField("HLS Segments length", blank=True, null=True)
+    format = models.CharField("Output Format", max_length=255)
+    encryption_key = models.CharField("Encryption Key", max_length=1024, null=True)
+    key_url = models.CharField("Encryption Key URL", max_length=1024, null=True)
+
+    class Meta:
+        ordering = ("-created",)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.slug = slugify(self.name)
+        super(JobTemplate, self).save(*args, **kwargs)
+
+
+class AbstractOutputPreset(TimeStampedModel):
+    VIDEO_ENCODERS = (("h264", "H244"), ("hevc", "HEVC"))
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField("Output Name", max_length=255)
+    video_encoder = models.CharField("Video Encoder", max_length=100, default="h264", choices=VIDEO_ENCODERS)
+    video_bitrate = models.PositiveIntegerField("Video Bitrate")
+    video_preset = models.CharField("Video Preset", max_length=100, default="faster")
+    audio_encoder = models.CharField("Audio Encoder", max_length=100, default="aac")
+    audio_bitrate = models.PositiveIntegerField("Audio Bitrate", default=128000)
+    width = models.PositiveSmallIntegerField("Video Width")
+    height = models.PositiveSmallIntegerField("Video Height")
+
+    class Meta:
+        ordering = ("-created",)
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class OutputPreset(AbstractOutputPreset):
+    job_template = models.ForeignKey(JobTemplate, null=True, on_delete=models.SET_NULL)
