0272f6efc9600b16aeac90ee686ec68ac539a142
Karthik <karthik@testpress.in>
2020-09-02 10:31:24 +0530

    Add more test case

--- apps/jobs/models.py
+++ apps/jobs/models.py
@@ -40,6 +40,11 @@ class Job(TimeStampedModel, TimeFramedModel):
     def __str__(self):
         return f"JOB {self.id} - {self.get_status_display()}"
 
+    def update_progress(self):
+        progress_dict = Output.objects.filter(job_id=self.id).aggregate(models.Avg("progress"))
+        self.progress = progress_dict["progress__avg"]
+        self.save()
+
 
 class Output(AbstractOutputPreset):
     STATUS = Choices(
