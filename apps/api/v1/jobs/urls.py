e84c47f9b83ab5db15c3b6b056d2fe7663b54ec1
Karthik <karthik@testpress.in>
2020-09-07 09:36:25 +0530

    Add cancel and get job info view

--- apps/api/v1/jobs/urls.py
+++ apps/api/v1/jobs/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 
-from .views import CreateJobView, JobsView
+from .views import CreateJobView, JobsView, job_info_view, cancel_job
 
-urlpatterns = [path("create/", CreateJobView.as_view()), path("", JobsView.as_view())]
+urlpatterns = [
+    path("create/", CreateJobView.as_view()),
+    path("", JobsView.as_view()),
+    path("<uuid:factory_id>/", job_info_view),
+    path("cancel/", cancel_job),
+]
--- apps/api/v1/jobs/views.py
+++ apps/api/v1/jobs/views.py
@@ -1,6 +1,9 @@
 from rest_framework.generics import CreateAPIView, ListAPIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.decorators import api_view
+
+from django.shortcuts import get_object_or_404
 
 from apps.jobs.models import Job
 from .serializers import JobSerializer
@@ -25,3 +28,19 @@ class CreateJobView(CreateAPIView):
 class JobsView(ListAPIView):
     queryset = Job.objects.all()
     serializer_class = JobSerializer
+
+
+@api_view(["GET"])
+def job_info_view(request, job_id):
+    job = get_object_or_404(Job, id=job_id)
+    transcode_manager = VideoTranscodeManager(job)
+    return Response(data=transcode_manager.get_job_info(), status=status.HTTP_200_OK)
+
+
+@api_view(["POST"])
+def cancel_job(request):
+    job_id = request.data["job_id"]
+    job = get_object_or_404(Job, id=job_id)
+    transcode_manager = VideoTranscodeManager(job)
+    transcode_manager.stop()
+    return Response(status=status.HTTP_200_OK)
