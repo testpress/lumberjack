b3a1867ff9741d85dc49677ee4637203cb111e1f
Karthik <karthik@testpress.in>
2020-09-08 13:10:21 +0530

    Fix error in webhook notify if webhook url is not available

--- apps/jobs/model_mixins.py
+++ apps/jobs/model_mixins.py
@@ -2,4 +2,7 @@ class JobNotifierMixin:
     def notify_webhook(self):
         from .tasks import PostDataToWebhookTask
 
+        if not self.webhook_url:
+            return
+
         PostDataToWebhookTask.apply_async(args=(self.job_info, self.webhook_url))
