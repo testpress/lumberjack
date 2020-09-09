9824d986604b936759df50656cccafe5124e6178
Karthik <karthik@testpress.in>
2020-09-09 14:59:14 +0530

    Generate static files with collectstatic

--- lumberjack/lumberjack/settings.py
+++ lumberjack/lumberjack/settings.py
@@ -123,8 +123,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(PROJECT_ROOT, "static")
 
 
 TRANSCODED_VIDEOS_PATH = get_env_variable("TRANSCODED_VIDEOS_PATH")
