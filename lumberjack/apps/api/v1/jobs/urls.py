f2acf4a112cabc909878efafc4e419fdc84ab7a7
Karthik <karthik@testpress.in>
2020-09-10 09:25:59 +0530

    Fix Job info API URL

--- lumberjack/apps/api/v1/jobs/urls.py
+++ lumberjack/apps/api/v1/jobs/urls.py
@@ -5,6 +5,6 @@ from .views import CreateJobView, JobsView, job_info_view, cancel_job_view
 urlpatterns = [
     path("create/", CreateJobView.as_view()),
     path("", JobsView.as_view()),
-    path("<uuid:factory_id>/", job_info_view),
+    path("<uuid:job_id>/", job_info_view),
     path("cancel/", cancel_job_view),
 ]
