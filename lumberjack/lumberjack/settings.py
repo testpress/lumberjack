f3e237ed2c8db08cbf7f7fd6bb5ca05c8cc6b2ab
Karthik <karthik@testpress.in>
2020-09-17 08:41:08 +0530

    Add django_object_actions to installed apps

--- lumberjack/lumberjack/settings.py
+++ lumberjack/lumberjack/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "apps.jobs",
     "apps.presets",
     "rest_framework",
+    "django_object_actions",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
