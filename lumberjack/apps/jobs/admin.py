ccdcc19a9b52bc01804418c31db73183a92c6ac9
Karthik <karthik@testpress.in>
2020-11-18 12:55:23 +0530

    Display start time, end time, status and progress in admin panel

--- lumberjack/apps/jobs/admin.py
+++ lumberjack/apps/jobs/admin.py
@@ -28,6 +28,11 @@ class JobAdmin(DjangoObjectActions, admin.ModelAdmin):
         transcode_manager = VideoTranscodeManager(obj)
         transcode_manager.restart()
 
+    def get_status(self, obj):
+        return obj.get_status_display()
+
+    get_status.short_description = "Status"
+
     restart.label = "Restart"
     restart.short_description = "Restart this job"
     change_actions = ("restart",)
@@ -35,11 +40,18 @@ class JobAdmin(DjangoObjectActions, admin.ModelAdmin):
 
     search_fields = ["id"]
     list_filter = ["status", "template"]
+    list_display = ["id", "get_status", "progress", "created", "start", "end"]
 
 
 class OutputAdmin(admin.ModelAdmin):
+    def get_status(self, obj):
+        return obj.get_status_display()
+
+    get_status.short_description = "Status"
+
     search_fields = ["job__id"]
     list_filter = ["status"]
+    list_display = ["id", "get_status", "progress", "created", "start", "end"]
 
 
 admin.site.register(Job, JobAdmin)
