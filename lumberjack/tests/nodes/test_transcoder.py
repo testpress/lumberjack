57ca02556c134e9762e0465e9bfcfcf1ddf15ec1
Karthik <karthik@testpress.in>
2020-12-16 10:19:46 +0530

    Fix test cases

--- lumberjack/tests/nodes/test_transcoder.py
+++ lumberjack/tests/nodes/test_transcoder.py
@@ -2,6 +2,7 @@ from django.test import SimpleTestCase
 
 from apps.nodes.transcoder import TranscoderNode
 from apps.nodes.base import ProcessStatus
+from apps.ffmpeg.utils import mkdir
 
 
 class TestTranscoder(SimpleTestCase):
@@ -26,6 +27,9 @@ class TestTranscoder(SimpleTestCase):
                 "audio": {"codec": "aac", "bitrate": "48000"},
             },
         }
+
+    def setUp(self):
+        mkdir("tests/ffmpeg/data/1232")
 
     def test_process_status_should_be_running_on_starting_transcoder(self):
         transcoder = TranscoderNode(self.output_settings)
